3c803e8bed78c110f288473bd386dbafcf612761
Jonathan Fiss <fissjonathan@gmail.com>
2019-04-16 12:07:25 -0300

    atualizando com o material

--- app/src/main/java/br/edu/ifsul/vendas/activity/ProdutosActivity.java
+++ app/src/main/java/br/edu/ifsul/vendas/activity/ProdutosActivity.java
@@ -1,13 +1,18 @@
 package br.edu.ifsul.vendas.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SearchView;
+import android.widget.Toast;
 
+import com.google.android.gms.common.api.CommonStatusCodes;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -19,7 +24,9 @@ import java.util.List;
 
 import br.edu.ifsul.vendas.R;
 import br.edu.ifsul.vendas.adapter.ProdutosAdapter;
+import br.edu.ifsul.vendas.barcode.BarcodeCaptureActivity;
 import br.edu.ifsul.vendas.model.Produto;
+import br.edu.ifsul.vendas.setup.AppSetup;
 
 public class ProdutosActivity extends AppCompatActivity {
 
@@ -32,7 +39,15 @@ public class ProdutosActivity extends AppCompatActivity {
         setContentView(R.layout.activity_produtos);
 
         lvProdutos = findViewById(R.id.lv_produtos);
-        lvProdutos.setOnItemClickListener(New o);
+        lvProdutos.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(ProdutosActivity.this, ProdutosDetalheActivity.class);
+                intent.putExtra("position", position);
+                startActivity(intent);
+            }
+        });
 
         // Write a message to the database
         FirebaseDatabase database = FirebaseDatabase.getInstance();
@@ -45,14 +60,15 @@ public class ProdutosActivity extends AppCompatActivity {
 
                 Log.d(TAG, "Value is: " + dataSnapshot.getValue());
 
-                List<Produto> produtos = new ArrayList<>();
+                AppSetup.produtos = new ArrayList<>();
                 for(DataSnapshot ds : dataSnapshot.getChildren()){
                     Produto produto = ds.getValue(Produto.class);
-                    produtos.add(produto);
+                    produto.setKey(ds.getKey());
+                    AppSetup.produtos.add(produto);
                 }
 
                 //carrega os dados na View
-                lvProdutos.setAdapter(new ProdutosAdapter(ProdutosActivity.this, produtos));
+                lvProdutos.setAdapter(new ProdutosAdapter(ProdutosActivity.this, AppSetup.produtos));
 
             }
 
@@ -67,22 +83,74 @@ public class ProdutosActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_activity_produtos, menu);
         SearchView searchView = (SearchView) menu.findItem(R.id.menuitem_pesquisar).getActionView();
         searchView.setQueryHint(getString(R.string.hint_nome_searchview));
-        searchView.setOnQueryTextListener(new SearchView.OnCloseListener(){
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
-            public boolean onQueryTextSubmit(String query);
-            return true;
-        }
-            @Override
-                public boolean onQueryTextChange(String newText){
-            List<Produto> produtosTemp = new ArrayList<>();
-            for(Produto produto : produtos){
-                if (produto.getNome().contains(newText)){
-                    produtosTemp.add(produto);
-                }
-
+            public boolean onQueryTextSubmit(String query) {
+                return true;
             }
-        }
 
-        );
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                List<Produto> produtosTemp = new ArrayList<>();
+                for (Produto produto : AppSetup.produtos) {
+                    if (produto.getNome().contains(newText)) {
+                        produtosTemp.add(produto);
+                    }
+                }
+                lvProdutos.setAdapter(new ProdutosAdapter(ProdutosActivity.this, produtosTemp));
+                return true;
+            }
+        });
+    return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(Menu.Item item){
+        switch (item.getItemId()){
+            case R.id.menuitem_barcode:
+                Intent intent = new Intent(ProdutosActivity.this, BarcodeCaptureActivity.class);
+                intent.putExtra(BarcodeCaptureActivity.AutoFocus, true);
+                intent.putExtra(BarcodeCaptureActivity.UseFlash, false);
+                startActivityForResult(intent, RC_BARCODE_CAPTURE);
+                break;
+        }
+        return true;
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == RC_BARCODE_CAPTURE) {
+            if (resultCode == CommonStatusCodes.SUCCESS) {
+                if (data != null) {
+                    Barcode barcode = data.getParcelableExtra(BarcodeCaptureActivity.BarcodeObject);
+                    //Toast.makeText(this, barcode.displayValue, Toast.LENGTH_SHORT).show();
+                    Log.d(TAG, "Barcode read: " + barcode.displayValue);
+                    //localiza o produto na lista (ou não)
+                    boolean flag = true;
+                    int position = 0;
+                    for (Produto produto : AppSetup.produtos) {
+                        if (String.valueOf(produto.getCodigoDeBarras()).equals(barcode.displayValue)) {
+                            flag = false;
+                            Intent intent = new Intent(ProdutosActivity.this, ProdutoDetalheActivity.class);
+                            intent.putExtra("position", position);
+                            startActivity(intent);
+                            break;
+                        }
+                        position++;
+                    }
+                    if (flag) {
+                        Snackbar.make(findViewById(R.id.container_activity_produtos), "codigo de barras não cadastrado", Snackbar.LENGTH_LONG).show();
+                    }
+                } else {
+                    Toast.makeText(this, "Falha na leitura do código", Toast.LENGTH_SHORT).show();
+                    Log.d(TAG, "No barcode captured, intent data is null");
+                }
+            } else {
+                Toast.makeText(this, String.format(getString(R.string.barcode_error),
+                        CommonStatusCodes.getStatusCodeString(resultCode)), Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
+
---
+++ app/src/main/java/br/edu/ifsul/vendas/setup/AppSetup.java
@@ -0,0 +1,15 @@
+package br.edu.ifsul.vendas.setup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.edu.ifsul.vendas.model.Cliente;
+import br.edu.ifsul.vendas.model.ItemPedido;
+import br.edu.ifsul.vendas.model.Produto;
+
+public class AppSetup {
+
+    public static List<Produto> produtos = new ArrayList<>();
+    public static Cliente cliente = null;
+    public static List<ItemPedido> carrinho = new ArrayList<>();
+}
